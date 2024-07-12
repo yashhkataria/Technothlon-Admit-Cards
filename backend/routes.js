ef1fafaa55606b4cf4b636c64abb8046a9e71f7d
Yash <y.kataria@iitg.ac.in>
2024-07-12 19:22:17 +0530

    another try

--- backend/routes.js
+++ backend/routes.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
-const { getOnlineTeams, getOfflineTeams, getCentreInfo, createAdmitCard, getAdmitCard } = require('./controllers');
+const { createAdmitCard, getAdmitCard } = require('./controllers');
 
-router.get('/online-teams', getOnlineTeams);
-router.get('/offline-teams', getOfflineTeams);
-router.get('/centre-info', getCentreInfo);
+router.get('/', (req, res) => {
+    res.send('Admit card api hello!');
+})
 router.post('/create-admit-card', createAdmitCard);
 router.get('/get-admit-card', getAdmitCard);
 
