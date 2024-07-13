7123ec28b63e9dd0db6c130eca3af579724518d6
Yash <y.kataria@iitg.ac.in>
2024-07-13 18:28:52 +0530

    mongo print

--- backend/connectDB.js
+++ backend/connectDB.js
@@ -9,6 +9,7 @@ const connectDB = async () => {
         useNewUrlParser: true,
         useUnifiedTopology: true,
         });
+        console.log(process.env.MONGO_URI);
         console.log('MongoDB connection SUCCESS');
     } catch (error) {
         console.error('MongoDB connection FAIL');
