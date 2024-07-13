0c0e0b844b8ed4e03d29a9e7a9973f799bcc2dba
Yash <y.kataria@iitg.ac.in>
2024-07-13 12:49:02 +0530

    once more

--- backend/server.js
+++ backend/server.js
@@ -8,18 +8,10 @@ dotenv.config();
 
 const app = express();
 
-const allowedOrigins = ['https://technothlon-admit-cards.vercel.app', 'http://localhost:3000'];
-
 const corsOptions = {
-  origin: function (origin, callback) {
-    if (allowedOrigins.includes(origin) || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
+  origin: '*',
   credentials: true
-};
+}
 
 app.use(cors(corsOptions));
 app.use(express.json());
@@ -30,13 +22,7 @@ app.get('/', (req, res) => {
   res.send('Admit card hello!');
 });
 
-app.use('/api', (req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://technothlon-admit-cards.vercel.app");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  res.header("Access-Control-Allow-Credentials", "true");
-  next();
-}, router);
+app.use('/api', router);
 
 const PORT = process.env.PORT || 4500;
 
