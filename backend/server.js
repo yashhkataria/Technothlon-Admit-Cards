fe6fc6e8d261ac593e1e9186b06f03ff28e6bad3
Yash <y.kataria@iitg.ac.in>
2024-07-13 17:37:56 +0530

    another change

--- backend/server.js
+++ backend/server.js
@@ -9,9 +9,17 @@ dotenv.config();
 const app = express();
 
 const corsOptions = {
-  origin: ['http://localhost:3000', 'https://technothlon-admit-cards.vercel.app/443'],
+  origin: '*',
   credentials: true
 }
+
+app.use(function(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
 
 app.use(cors(corsOptions));
 app.use(express.json());
