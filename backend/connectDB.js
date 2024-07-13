2cee762e74f2a679b234296db5c2fd31a5ad2647
Yash <y.kataria@iitg.ac.in>
2024-07-13 18:31:49 +0530

    url

--- backend/connectDB.js
+++ backend/connectDB.js
@@ -4,12 +4,13 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const connectDB = async () => {
+    console.log(process.env.MONGO_URI);
+    
     try {
         await mongoose.connect(process.env.MONGO_URI, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
         });
-        console.log(process.env.MONGO_URI);
         console.log('MongoDB connection SUCCESS');
     } catch (error) {
         console.error('MongoDB connection FAIL');
