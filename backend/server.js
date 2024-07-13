2b63a1340c1c163ae67db80a034766e2a051cf1a
Yash <y.kataria@iitg.ac.in>
2024-07-13 11:43:50 +0530

    again cors

--- backend/server.js
+++ backend/server.js
@@ -8,26 +8,10 @@ dotenv.config();
 
 const app = express();
 
-const allowedOrigins = [
-  'http://localhost:3000',
-  'https://technothlon-admit-cards.vercel.app'
-];
-
 const corsOptions = {
-  origin: (origin, callback) => {
-      if (allowedOrigins.includes(origin) || !origin) {
-          callback(null, true);
-      } else {
-          callback(new Error('Not allowed by CORS'));
-      }
-  },
+  origin: "https://technothlon-admit-cards.vercel.app",
   credentials: true
-};
-
-// const corsOptions = {
-//   origin: "https://technothlon-admit-cards.vercel.app",
-//   credentials: true
-// }
+}
 
 app.use(cors(corsOptions));
 app.use(express.json());
